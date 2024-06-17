bf91af39713e70a8f0a2f61b1301bcda80b7092e
Acr777 <115130508+Acr777@users.noreply.github.com>
2024-06-17 12:42:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,31 @@
 self.addEventListener('push', function(event) {
-  const data = event.data.json();
-  const options = {
-      body: data.body,
-      icon: 'notification_icon.png',
-      data: data.url
-  };
-  event.waitUntil(
-      self.registration.showNotification(data.title, options)
-  );
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: '2.png', // You can specify a notification icon here
+        data: {
+            url: data.url
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-      clients.openWindow(event.notification.data)
-  );
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if (client.url == event.notification.data.url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
 });
+
